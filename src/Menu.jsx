6a25d00a548844b5067019c68aea371345a40d1e
Kennert777 <rm94720@estudante.fieb.edu.br>
2025-06-05 11:16:18 +0000

    menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,37 +1,34 @@
-const Menu = () =>{
+import'./Menu.css'
+const Menu = () => {
 
     return(
 
         <div className="Menu">
         <ul className="nav">
         
-        <li className="nav-itm">
+        <li className="nav-item">
         <a className="nav-link active" href="/home">
                 Home
         </a>
         
         </li>
-         <li className="nav-itm">
+         <li className="nav-item">
         <a className="nav-link active" href="/pokeflex">
                 PokeFlex
         </a>
         </li>
         
-         <li className="nav-itm">
-        <a className="nav-link active" href="/pokegrid>
-                PokeGrid
         
+         <li className="nav-item">
+        <a className="nav-link active" href="/pokegrid">
+                PokeGrid
         </a>
         </li>
-        
-        
-        
-        
-        
+
         
         </ul>
         </div>
     )
-}
+    }
 
 export default Menu
